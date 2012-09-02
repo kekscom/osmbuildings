e461af9282e2afc858a335b7dff37996327c00e7
Jan Marsch <jama@keks.com>
2012-09-02 23:01:47 +0200

    roof shapes added: pyramidal, cone, dome

--- dist/buildings-debug.js
+++ dist/buildings-debug.js
@@ -702,16 +702,146 @@ var Color = (function () {
                 // fill roof and optionally stroke it
                 context.fillStyle = (item[COLOR] ? item[COLOR][1].adjustAlpha(zoomAlpha) : roofColorAlpha) + '';
                 drawShape(roof, strokeRoofs);
+                drawRoof(roof);
             }
         }
 
-//        function debugMarker(x, y, color, size) {
-//            context.fillStyle = color || '#ffcc00';
-//            context.beginPath();
-//            context.arc(x, y, size || 3, 0, PI*2, true);
-//            context.closePath();
-//            context.fill();
-//        }
+
+
+
+
+function circle(x, y, diameter, stroke) {
+    ellipse(x, y, diameter, diameter, stroke);
+}
+
+function ellipse(x, y, w, h, stroke) {
+    var
+        w2 = w / 2, h2 = h / 2,
+        hB = w2 * 0.5522848,
+        vB = h2 * 0.5522848,
+        eX = x + w2, eY = y + h2,
+        mX = x, mY = y
+    ;
+
+    x -= w2;
+    y -= h2;
+
+    context.beginPath();
+    context.moveTo(x, mY);
+    context.bezierCurveTo( x,      mY - vB, mX - hB,  y,      mX, y);
+    context.bezierCurveTo(mX + hB,       y, eX,      mY - vB, eX, mY);
+    context.bezierCurveTo(eX,      mY + vB, mX + hB, eY,      mX, eY);
+    context.bezierCurveTo(mX - hB,      eY,  x,      mY + vB,  x, mY);
+    context.closePath();
+    context.fill();
+    if (stroke) {
+        context.stroke();
+    }
+}
+
+        function drawRoof2(points) {
+            context.fillStyle = 'rgba(240,0,0,0.25)';
+            context.strokeStyle = strokeColor.adjustAlpha(zoomAlpha) + '';
+
+            var
+                h = 20,
+                center = [
+                    (points[0] + points[2] + points[4] + points[6]) / 4,
+                    (points[1] + points[3] + points[5] + points[7]) / 4
+                ],
+                apex = project(center[0], center[1], CAM_Z / (CAM_Z - h))
+            ;
+
+            var d = 65;
+            circle(center[0], center[1], d, d, true);
+
+            context.beginPath();
+            context.moveTo(center[0] - d / 2, center[1]);
+            context.lineTo(apex.x, apex.y);
+            context.lineTo(center[0] + d / 2, center[1]);
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(center[0], center[1] - d / 2);
+            context.lineTo(apex.x, apex.y);
+            context.lineTo(center[0], center[1] + d / 2);
+            context.stroke();
+        }
+
+
+        function drawRoof(points) {
+            context.fillStyle = 'rgba(240,0,0,0.25)';
+            context.strokeStyle = strokeColor.adjustAlpha(zoomAlpha) + '';
+
+            var
+                h = 10,
+                center = [
+                    (points[0] + points[2] + points[4] + points[6]) / 4,
+                    (points[1] + points[3] + points[5] + points[7]) / 4
+                ],
+                apex = project(center[0], center[1], CAM_Z / (CAM_Z - h))
+            ;
+
+            var d = 65;
+            circle(center[0], center[1], d, d, true);
+            debugMarker(apex.x, apex.y);
+
+            var d2 = d / 2;
+            var w = center[0] - d2;
+            var e = center[0] + d2;
+            var n = center[1] - d2;
+            var s = center[1] + d2;
+
+            context.beginPath();
+            context.moveTo(w, center[1]);
+            context.bezierCurveTo((apex.x + w) / 2.05, center[1] + (apex.y - center[1]) * 1.5, (apex.x + e) / 1.95, center[1] + (apex.y - center[1]) * 1.5, e, center[1]);
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(center[0], n);
+            context.bezierCurveTo(center[0] + (apex.x - center[0]) * 1.5, (apex.y + n) / 2.05, center[0] + (apex.x - center[0]) * 1.5, (apex.y + s) / 1.95, center[0], s);
+            context.stroke();
+        }
+
+        function drawRoof1(points) {
+            context.fillStyle = 'rgba(240,0,0,0.25)';
+            var
+                h = 20 + 10,
+                center = [
+                    (points[0] + points[2] + points[4] + points[6]) / 4,
+                    (points[1] + points[3] + points[5] + points[7]) / 4
+                ],
+                apex = project(center[0], center[1], CAM_Z / (CAM_Z - h))
+            ;
+            drawShape([
+                points[0], points[1],
+                points[2], points[3],
+                apex.x, apex.y
+            ], true);
+            drawShape([
+                points[2], points[3],
+                points[4], points[5],
+                apex.x, apex.y
+            ], true);
+            drawShape([
+                points[4], points[5],
+                points[6], points[7],
+                apex.x, apex.y
+            ], true);
+            drawShape([
+                points[6], points[7],
+                points[0], points[1],
+                apex.x, apex.y
+            ], true);
+        }
+
+        function debugMarker(x, y, color, size) {
+            context.fillStyle = color || '#ffcc00';
+            context.beginPath();
+            context.arc(x, y, size || 3, 0, PI*2, true);
+            context.closePath();
+            context.fill();
+        }
 
         function drawShape(points, stroke) {
             if (!points.length) {
