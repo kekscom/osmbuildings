487234b305c371fa2c3ef1fc28fe12356f146047
Jan Marsch <jama@keks.com>
2012-10-09 23:19:32 +0200

    adding proper credits

--- dist/OpenLayers.Layer.Buildings-debug.js
+++ dist/OpenLayers.Layer.Buildings-debug.js
@@ -826,6 +826,10 @@ var Color = (function () {
 
 //****** file: OpenLayers.js ******
 
+/**
+ * basing on a pull request from Jérémy Judéaux (https://github.com/Volune)
+ */
+
 OpenLayers.Layer.Buildings = OpenLayers.Class(OpenLayers.Layer, {
 
     CLASS_NAME: 'OpenLayers.Layer.Buildings',
