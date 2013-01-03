fdfdac0435bd51ab321b67553a5ce83eadb86f7f
Jan Marsch <jama@keks.com>
2013-01-03 01:44:27 +0100

    server: height scale adjusted

--- server/config.sample.php
+++ server/config.sample.php
@@ -2,7 +2,7 @@
 
 $offsetX     = 0;   // adjust x pixel values
 $offsetY     = 0;   // adjust y pixel values
-$heightScale = 3;   // scale building height by this factor
+$heightScale = 1.2;   // scale building height by this factor
 $tileSize    = 256; // tile size, usually 256
 $maxZoom     = 18;  // max. zoom level in your tile set
 $coordsOrder = "lat,lon"; // the order of coordinates in your data
