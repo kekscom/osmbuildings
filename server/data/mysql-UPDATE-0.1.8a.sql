8869f2e357ec9af2515d5a43c5a3e0255028c240
Jan Marsch <jama@keks.com>
2013-02-28 00:29:35 +0100

    sql regions dump enhanced

--- server/data/mysql-UPDATE-0.1.8a.sql
+++ server/data/mysql-UPDATE-0.1.8a.sql
@@ -1,12 +1,14 @@
-ALTER TABLE `buildings` ADD `min_height` int(3) DEFAULT NULL AFTER `height`;
-ALTER TABLE `buildings` CHANGE region region_id int(8) UNSIGNED NOT NULL;
+ALTER TABLE `buildings` ADD `min_height` INT(3) DEFAULT NULL AFTER `height`;
+ALTER TABLE `buildings` CHANGE region region_id INT(8) UNSIGNED NOT NULL;
 
 CREATE TABLE IF NOT EXISTS `regions` (
   `id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT,
   `bbox` polygon NOT NULL,
-  `buildings` int(16) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `created` datetime NOT NULL,
+  `num_buildings` INT(16) UNSIGNED NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `source` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `timezone` INT(4) NULL,
   PRIMARY KEY (`id`),
   KEY `bbox` (`bbox`(32))
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
