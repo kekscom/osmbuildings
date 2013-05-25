9bf9b60f96e07a6d75fdfc7e3480297ab6bb15bf
Jan Marsch <jama@keks.com>
2013-05-25 22:42:46 +0200

    server: curl option disabled for certain php configs

--- server/source/CartoDB.php
+++ server/source/CartoDB.php
@@ -45,7 +45,7 @@ class Source_CartoDB extends Source_Abstract {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+//        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
         $res = curl_exec($ch);
         curl_close($ch);
         if ($res) {
