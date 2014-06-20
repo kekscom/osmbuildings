ae5538bb47cc0fe0dcc215fe7ff3286152b0c828
Jan Marsch <jama@keks.com>
2014-06-20 22:43:47 +0200

    reorg

--- src/adapter.js
+++ src/adapter.js
@@ -61,3 +61,41 @@ function onZoomEnd(e) {
   Data.update(); // => fadeIn()
   Layers.render(true);
 }
+
+
+
+
+
+
+
+var self = this;
+function onDeviceMotion(e) {
+	self.setCamOffset(e.x, e.y);
+    render();
+}
+
+if (window.DeviceMotionEvent) {
+	var minMovement = 2, abs = Math.abs;
+	window.addEventListener('devicemotion', function(e) {
+		var t;
+		if ((e = e.accelerationIncludingGravity || e.acceleration)) {
+			if (abs(e.x) > minMovement || abs(e.y) > minMovement || abs(e.z) > minMovement) {
+				switch (window.orientation) {
+					case  -90: t = e.x; e.x =  e.y; e.y = -t; break;
+					case   90: t = e.x; e.x = -e.y; e.y =  t; break;
+					case  180:
+					case -180: e.x *= -1; e.y *= -1; break;
+				}
+				onDeviceMotion({ x:-e.x*10, y:-e.y*10 });
+			}
+		}
+    });
+}
+
+window.addEventListener('mousemove', function(e) {
+	var minMovement = 2, abs = Math.abs;
+    var x = width/2-e.x, y = height-e.y;
+    if (abs(x) > minMovement || abs(y) > minMovement) {
+        onDeviceMotion({ x:-x/3, y:-y/3 });
+    }
+});
