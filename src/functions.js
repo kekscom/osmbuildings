82f5ebddfd2501c4397df85d7f2e869a55346f81
Jan Marsch <jama@keks.com>
2015-03-11 12:06:53 +0100

    comma fix

--- src/functions.js
+++ src/functions.js
@@ -11,13 +11,14 @@ function pixelToGeo(x, y) {
   var res = {};
   x /= MAP_SIZE;
   y /= MAP_SIZE;
-  res[LAT] = y <= 0  ? 90 : y >= 1 ? -90 : deg(2 * atan(exp(PI * (1 - 2*y))) - HALF_PI),
+  res[LAT] = y <= 0  ? 90 : y >= 1 ? -90 : deg(2 * atan(exp(PI * (1 - 2*y))) - HALF_PI);
   res[LON] = (x === 1 ?  1 : (x%1 + 1) % 1) * 360 - 180;
   return res;
 }
 
 function geoToPixel(lat, lon) {
-  var latitude  = min(1, max(0, 0.5 - (log(tan(QUARTER_PI + HALF_PI * lat / 180)) / PI) / 2)),
+  var
+    latitude = min(1, max(0, 0.5 - (log(tan(QUARTER_PI + HALF_PI * lat / 180)) / PI) / 2)),
     longitude = lon/360 + 0.5;
   return {
     x: longitude*MAP_SIZE <<0,
