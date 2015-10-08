c3be6183af3f8867ce791d7fa087e3aeb8ee0eb8
Jan Marsch <jama@jama.fritz.box>
2015-10-09 01:01:36 +0200

    very basic OL3 support added

--- src/engines/OL3.js
+++ src/engines/OL3.js
@@ -1,13 +1,15 @@
+
 var osmb = function(map) {
-    this.map = map;
-    this.maxExtent = [ -20037508.34, -20037508.34, 20037508.34, 20037508.34]; // MaxExtent of layer
-    try {
-        this.setMap(map);
-        map.addLayer(this);
-    } catch (e) {
-        console.log(e);
-    }
+  this.map = map;
+  this.maxExtent = [ -20037508.34, -20037508.34, 20037508.34, 20037508.34]; // MaxExtent of layer
+  try {
+    this.setMap(map);
+    map.addLayer(this);
+  } catch (e) {
+    console.log(e);
+  }
 };
+
 ol.inherits(osmb, ol.layer.Vector);
 
 var proto = osmb.prototype = ol.layer.Layer ? new  ol.layer.Vector({source: new ol.source.Vector( { projection: ol.proj.get('EPSG:900913') } )}) : { };
@@ -19,10 +21,10 @@ proto.setOrigin = function() {
     res = map.getView().getResolution(),
     ext = this.maxExtent,
     x = (origin[0] - ext[0]) / res <<0,
-    y = (ext[3] - origin[1]) / res <<0;      
+    y = (ext[3] - origin[1]) / res <<0;
     setOrigin({ x:x, y:y });
   } catch (e) {
-      console.log(e);
+    console.log(e);
   }
 };
                                                                             
@@ -39,12 +41,11 @@ proto.setMap = function(map) {
       onClick({ feature:id, lat:geo[0], lon:geo[1] });
     }
   });
-  
+
   this.on('precompose', function(e) {
-      setZoom(map.getView().getZoom());
-      scope.setOrigin();
-      Data.resetItems();
-      Data.update();
+    setZoom(map.getView().getZoom());
+    scope.setOrigin();
+    Data.resetItems();
+    Data.update();
   });
-  
 };
